8e534285bf401b3003692fa43e02a8fc45ae1704
Alex Pritchard <alexbeet1234@gmail.com>
2021-08-29 09:36:52 +0800

    created new airline component and added styled-components

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect, Fragment } from 'react'
+import axios from 'axios'
+import Airline from './Airline'
 
 const Airlines = () => {
+  const [airlines, setAirlines] = useState([])
+
+  useEffect(() => {
+    // get all airlines from api
+    // update airlines in our state
+
+    axios.get('/api/v1/airlines.json')
+    .then( resp => {
+      setAirlines(resp.data.data)
+    })
+    .catch( resp => console.log(resp) )
+  }, [airlines.length]) 
+
+  const grid = airlines.map( item => {
+    return (
+      <Airline 
+        key={item.attributes.name}
+        attributes={item.attributes}
+      />
+    )
+  })
+
   return (
-    <div>This is the Airlines#index view for our app.</div>
+    <div className="home">
+      <div className="header">
+        <h1>OpenFlights</h1>
+        <div className="subheader">Honest, unbaised airline reviews.</div>
+      </div>
+      <div className="grid">
+        <ul>{grid}</ul>
+      </div>
+    </div>
   )
 }
 
